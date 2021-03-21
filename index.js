71d4bb4edb42f0f5fc9711d8736a4ea07e20a21a
ResponsoMundus <gamesluook_hi@126.com>
2021-03-21 14:04:51 -0500

    Update

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
+var old_url;
+
 $(document).ready(function() {
     const urlParams = new URLSearchParams(window.location.search)
     const lang = urlParams.get('lang')
-    
+    old_url = $(".navbar-brand").attr("href")
+
     // alert(lang)
     if (lang == 'zh') {
         $("#lang").prop('checked', true)
         $(".english").hide()
         $(".chinese").show()
+        $(".navbar-brand").attr("href", old_url + "?lang=zh")
     }
 })
 
 $(function() {
     $("#lang").on('change', function() {
         // alert(this.checked)
-
         if (this.checked) {
             $(".english").hide()
             $(".chinese").show()
+            $(".navbar-brand").attr("href", old_url + "?lang=zh")
         } else {
             $(".english").show()
             $(".chinese").hide()
+            $(".navbar-brand").attr("href", old_url)
         }
     })
 })
